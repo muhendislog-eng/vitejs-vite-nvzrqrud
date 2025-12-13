69f4812e361bcc7c9c2e4df8995d9abeef72177a
muhendislog-eng <muhendislog@gmail.com>
2025-12-13 03:35:59 +0300

    1341

--- src/App.tsx
+++ src/App.tsx
@@ -376,7 +376,25 @@ export default function App() {
         </div>
       )}
     </div>
+    
+export default function App() {
+  // ... state ve fonksiyonlar
 
+  return (
+    <div className="min-h-screen bg-slate-100 font-sans text-slate-800 pb-0 relative w-full overflow-x-hidden flex flex-col"> {/* pb-0 ve flex-col eklendi */}
+       
+       {/* ... Header, Main vb. ... */}
+
+       <main className="flex-1 w-full px-4 py-6 ...">
+          {/* ... İçerikler ... */}
+       </main>
+
+       <Footer /> {/* YENİ EKLENDİ */}
+
+       {/* ... Modallar ... */}
+    </div>
+  );
+}
   
   );
 }
